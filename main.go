dd28b6d5bf5a7a09c301a20f83c1942d87d0c2e2
Ahmet <AhmetTK4@users.noreply.github.com>
2025-07-25 23:13:33 +0300

    Add stats endpoint and fix redirect tracking with Redis

--- main.go
+++ main.go
@@ -33,11 +33,41 @@ func main() {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
 			return
 		}
-
 		c.JSON(http.StatusOK, gin.H{
 			"short_url": "http://localhost:8080/g/" + shortCode,
 		})
 	})
 
+	r.GET("/g/:shortCode", func(c *gin.Context) {
+		shortCode := c.Param("shortCode")
+
+		longURL, err := storage.RDB.Get(storage.Ctx, shortCode).Result()
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Short URL not found"})
+			return
+		}
+
+		storage.RDB.Incr(storage.Ctx, "clicks:"+shortCode)
+
+		c.Redirect(http.StatusFound, longURL)
+	})
+
+	r.GET("/api/stats/:shortCode", func(c *gin.Context) {
+		shortCode := c.Param("shortCode")
+
+		count, err := storage.RDB.Get(storage.Ctx, "clicks:"+shortCode).Result()
+		if err != nil {
+			count = "0"
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"short_code": shortCode,
+			"clicks":     count,
+		})
+
+	})
+
 	r.Run(":8080")
+
 }
